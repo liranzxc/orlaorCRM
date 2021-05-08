901be2849bdb1355e02f84c2e6f16050fa130319
lirannh <lirannh@aidock.net>
2021-05-08 21:34:38 +0300

    add gmail

--- orlaor-server-crm/src/env.ts
+++ orlaor-server-crm/src/env.ts
@@ -1,9 +1,12 @@
 export const ENV = {
   MONGODB_USERNAME: 'xx',
-  MONGODB_PASSWORD: 'xx',
-  MONGODB_DB: `xx`,
-  MONGODB_CLUSTER_NAME: 'xxxx',
+  MONGODB_PASSWORD: 'xxx',
+  MONGODB_DB: `xxx`,
+  MONGODB_CLUSTER_NAME: 'xxx',
   API_KEY: 'xxx',
+  EMAIL_AUTH_GMAIL: 'xxx',
+  PASS_AUTH_GMAIL: 'xx',
+  CLIENT_EMAIL: 'xxx',
 };
 
 export const MONGO_URL = `mongodb+srv://${ENV.MONGODB_USERNAME}:${ENV.MONGODB_PASSWORD}@${ENV.MONGODB_CLUSTER_NAME}/${ENV.MONGODB_DB}?retryWrites=true&w=majority`;
